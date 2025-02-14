64f1802acb8c8220a632414123b418f5e8e12601
Victor Orrios <victor.orrios.b@gmail.com>
2025-02-14 10:08:46 +0100

    static adpater

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,12 +7,26 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [vitePreprocess()],
-	
+
+	/*
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		paths : {
+			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+
+		}
 	}
+	*/
+	kit: {
+		adapter: adapter({
+			pages: 'sveltekit-github-pages', // Carpeta de salida para GitHub Pages
+			assets: 'sveltekit-github-pages', // Carpeta de assets estáticos
+			fallback: 'index.html' // Necesario para SPA en GitHub Pages
+		})
+	}
+
 };
 export default config;
---
+++ sveltekit-github-pages/_app/immutable/entry/start.DlNBO-4s.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/HdouT93o.js";export{t as start};
