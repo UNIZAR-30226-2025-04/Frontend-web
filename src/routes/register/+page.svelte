684b76198a26c23ce1935f199ae20e928306b1bd
Ruben123454 <869039@unizar.es>
2025-02-27 15:16:12 +0100

    register form with password check

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,17 +1,24 @@
-
-
 <script lang="ts">
 
 	let email = '';
-	let passwd = '';
-	let remember = false;
+	let username = '';
+	let passwd1 = '';
+	let passwd2 = '';
+	let errorMessage = "";
   
 	// Test handler function
 	function login(event: SubmitEvent): void {
 	  event.preventDefault();
+
+	  if(passwd1 != passwd2){
+			errorMessage = "Oops! Your passwords don't match. Please re-enter them.";
+			return;
+	  }
+
 	  console.log("Email:", email);
-	  console.log("Password:", passwd);
-	  console.log("Remember me:", remember);
+	  console.log("Username:", username);
+	  console.log("Password:", passwd1);
+	  console.log("Password:", passwd2);
 	}
 
 </script>
@@ -19,8 +26,8 @@
 
 
 <div class="login"> 
-  <h1>Welcome back!</h1>
-  <p>Don't have an account yet? <a href="register">Create account</a></p>
+  <h1 style="font-size: 30px;">Create an account on Nøgler!</h1>
+  <p style="font-size: 20px;">Already have an account? <a href="login">Sing in</a></p>
   
   <div class="login-form">
     <form id="login-form" on:submit={login}> 
@@ -28,32 +35,41 @@
       <input 
         type="email" 
         id="email" 
-        bind:value={email} 
-        name="email" 
+        bind:value={email}
         placeholder="your-email@example.com"
+        required
+      >
+
+	  <label for="username">Username</label>
+      <input 
+        type="text"
+        id="username" 
+        bind:value={username} 
+        placeholder="Your username"
         required
       >
       
       <label for="passwd">Password</label>
       <input 
         type="password" 
-        id="passwd" 
-        bind:value={passwd} 
-        name="passwd" 
+        id="passwd1" 
+        bind:value={passwd1} 
         placeholder="Your password"
         required
       >
-      
-      <label class="flex items-center space-x-2">
-        <input 
-          class="checkbox" 
-          type="checkbox" 
-          id="remember" 
-          bind:checked={remember} 
-          name="remember"
-        >
-        <span>Remember me</span>
-      </label>
+
+	  <label for="passwd">Repeat your password</label>
+      <input 
+        type="password" 
+        id="passwd2" 
+        bind:value={passwd2} 
+        placeholder="Your password"
+        required
+      >
+
+	  {#if errorMessage}
+		<span style="font-size: 18px; color: red;">{errorMessage}</span>
+	  {/if}
       
       <button type="submit">Login</button>
     </form>
@@ -71,7 +87,7 @@
 	.login {
 	  	text-align: center;
 	  	color: rgb(255, 255, 255);
-        margin-top: 150px;
+        margin-top: 120px;
 	}
   
 	.login h1 {
@@ -84,7 +100,7 @@
   
 	.login-form {
 	  	margin-top: 20px;
-	  	background: rgba(255, 255, 255, 0.575);
+	  	background: rgba(255, 255, 255, 0.863);
 	  	padding: 30px;
 	  	border-radius: 10px;
 	  	box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
@@ -92,6 +108,7 @@
 	  	width: 550px;
 	  	display: flex;
 	  	flex-direction: column;
+		font-size: 18px;
 	}
   
 	.login-form label {
@@ -99,8 +116,7 @@
 	  	color: #333333;
 	}
   
-	input[type="email"],
-	input[type="password"] {
+	input {
 	  	width: 100%;
 	  	padding: 10px;
 	  	margin-top: 5px;
@@ -110,13 +126,13 @@
 	  	color: #333333;
 	  	background-color: #ffffff;
 	  	box-sizing: border-box;
+		font-size: 17px;
 	}
   
 	button {
 	  	background-color: #7d2e2e;
 	  	color: white;
 	  	padding: 10px;
-	  	border: none;
 	  	border-radius: 15px;
 	  	cursor: pointer;
 	  	margin-top: 20px;
