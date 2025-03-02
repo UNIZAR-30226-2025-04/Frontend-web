20cccab0320868bb3af009056fe007d0ddbe3fc7
Ruben123454 <869039@unizar.es>
2025-03-02 20:46:20 +0100

    lobby for guest

--- src/routes/lobby/+page.svelte
+++ src/routes/lobby/+page.svelte
@@ -12,9 +12,11 @@
   let publicString = "PUBLIC"; // String to show if the lobby is public or private
   let publicValue = true; // Boolean to know if the lobby is public or private
   let code = "1234"; // Code of the lobby
+  let host = true; // Boolean to know if the player is the host
   
   // Function to switch the public value
   function onSwitchPublic(){
+    if(host)
     publicString = publicValue ? "PRIVATE" : "PUBLIC";
     publicValue = !publicValue;
     console.log(publicValue);
@@ -40,6 +42,7 @@
     key: number
     username: string
     icon: number
+    host: boolean
   };
 
   // Local player
@@ -48,14 +51,14 @@
 
   // List of players, includes testing data for viewing purposes
   let players: Player[] = [
-    {username: username, icon: avatar, key:0},
-    {username: "Player2", icon: 2, key:1},
-    {username: "Player3", icon: 3, key:2},
-    {username: "Player4", icon: 4, key:3},
-    {username: "Player5", icon: 5, key:4},
-    {username: "Player6", icon: 6, key:5},
-    {username: "Player7", icon: 7, key:6},
-    {username: "Player8", icon: 8, key:7},
+    {username: username, icon: avatar, key:0, host: true},
+    {username: "Player2", icon: 2, key:1, host: false},
+    {username: "Player3", icon: 3, key:2, host: false},
+    {username: "Player4", icon: 4, key:3, host: false},
+    {username: "Player5", icon: 5, key:4, host: false},
+    {username: "Player6", icon: 6, key:5, host: false},
+    {username: "Player7", icon: 7, key:6, host: false},
+    {username: "Player8", icon: 8, key:7, host: false},
   ];
 
   /**
@@ -74,7 +77,7 @@
    * @param icon
    */
   function addPlayer(username:string, icon:number){
-    let newUser:Player = {username:username,icon:icon,key:players.length};
+    let newUser:Player = {username:username,icon:icon,key:players.length,host:false};
     players = [...players,newUser]
   }
 
@@ -98,15 +101,22 @@
     <div animate:flip={{ duration: 500, easing: cubicOut }} class="block card card-hover p-4 h-[28vmin] w-[40vmin] shadow-xl space-y-6  text-left" style="flex: 0 0 40vmin;"> 
       <AvatarDisplay icon={player.icon} width={100}/>
       <h1 class="text-[3vmin]">{player.username}</h1>
-      {#if index != 0}
-      <button class="btn btn-lg variant-filled mt-[30%]" on:click={() => onKickPlayer(index)}>Kick</button>
+      {#if player.host}
+        <h1 class="text-green-500 text-[5vmin]">HOST</h1>
+      {/if}
+      {#if host}
+        {#if index != 0}
+        <button class="btn btn-lg variant-filled mt-[30%]" on:click={() => onKickPlayer(index)}>Kick</button>
+        {/if}
       {/if}
     </div>
   {/each}
 </div>
 
 <!-- Leave / Start button -->
- <div class="flex flex-row gap-[5vmin] mt-[1%] ml-[-47%]">
+{#if host}
+  <div class="flex flex-row gap-[5vmin] mt-[1%] ml-[-47%]">
   <button type="button" class="btn btn-lg variant-filled w-[40vmin] h-[7vmin] mt-[5%] ml-[5%]" on:click={onLeave}>Leave</button>
   <button type="button" class="btn btn-lg variant-filled w-[40vmin] h-[7vmin] mt-[5%] ml-[5%]" on:click={onStart}>Start</button>
- </div>
+  </div>
+{/if}
