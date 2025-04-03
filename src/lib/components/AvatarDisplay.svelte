880fc3cbd56e937e8ea6b54524890aabf2e0844f
Victor Orrios <victor.orrios.b@gmail.com>
2025-04-03 14:46:39 +0200

    Fixed bug in AvatarDisplay

--- src/lib/components/AvatarDisplay.svelte
+++ src/lib/components/AvatarDisplay.svelte
@@ -5,16 +5,28 @@
     export let width: number = 100;
     export let classW: string = "";
 
-    if (icon <= 0 || icon > avatarDirectory.length) {
-        icon = 1;
-    }
 </script>
 
-{#if classW !== ""}
-    {#if icon <= 0}
+{#if (icon <= 0 || icon > avatarDirectory.length)}
+    {#if classW !== ""}
+    <img
+        src="icons/missing.png"
+        alt="Missing"
+        style="border-radius: 100%;"
+        class={`w-${classW} aspect-square object-cover`}
+    />
+    {:else}
+    <img
+        src="icons/missing.png"
+        alt="Missing"
+        style="width: {width}px; height: {width}px; border-radius: 100%;"
+    />
+    {/if}
+{:else}
+    {#if classW !== ""}
         <img
-            src={avatarDirectory[0].link}
-            alt={avatarDirectory[0].name}
+            src={avatarDirectory[icon - 1].link}
+            alt={avatarDirectory[icon - 1].name}
             style="border-radius: 100%;"
             class={`w-${classW} aspect-square object-cover`}
         />
@@ -22,20 +34,7 @@
         <img
             src={avatarDirectory[icon - 1].link}
             alt={avatarDirectory[icon - 1].name}
-            style="border-radius: 100%;"
-            class={`w-${classW} aspect-square object-cover`}
+            style="width: {width}px; height: {width}px; border-radius: 100%;"
         />
     {/if}
-{:else if icon <= 0}
-    <img
-        src={avatarDirectory[0].link}
-        alt={avatarDirectory[0].name}
-        style="width: {width}px; height: {width}px; border-radius: 100%;"
-    />
-{:else}
-    <img
-        src={avatarDirectory[icon - 1].link}
-        alt={avatarDirectory[icon - 1].name}
-        style="width: {width}px; height: {width}px; border-radius: 100%;"
-    />
 {/if}
