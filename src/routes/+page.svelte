d1e19e0fdacf20df66790ceb9a6aba029dfe3ce9
Victor Orrios <victor.orrios.b@gmail.com>
2025-05-05 13:09:29 +0200

    Removed lore ipsum from root

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,8 @@
     import { onMount } from 'svelte';
     import { loginFetch } from '$lib/fetch/loginFetch';
     import { getDrawerStore, type DrawerSettings } from '@skeletonlabs/skeleton';
+    import GameCard from '$lib/components/GameCard.svelte';
+    import VoucherCard from '$lib/components/VoucherCard.svelte';
 
 	onMount(async () => {
 		// If user is cached and wants to be remembered we log in and get info
@@ -55,17 +57,11 @@
 <img class='logo' src='nogler2.png' alt="Nogler" style="margin-top: 2vh; height: 40vmin; width: auto"/>
 <!-- Game description -->
 <p class='description' style="font-size: max(2vmin,15px); margin-top: 4%; margin-right: 10%; margin-left: 10%; ">
-	Card game... Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-	sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-	<br> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-	nisi ut aliquip ex ea commodo consequat. 
+	Play against your friends in this cross platform Balatro inspired card game
 </p>
 <!-- Button: Start playing-->
 <a href="{base}/login" class="btn variant-filled p-[1.5vmin]" style="margin-top: 2%; font-size: max(2vmin,15px);">
 	<span>Login page</span>
-</a>
-<a href="{base}/home" class="btn variant-filled p-[1.5vmin] w-" style="margin-top: 2%; font-size: max(2vmin,15px);">
-	<span>Home</span>
 </a>
 
 <!-- Scroll how to play	-->
@@ -76,36 +72,33 @@
 
  
  <!-- Card 1: Cards -->
- <div class="card-hover" style="height: auto; width: 50vw; margin-top: 10%; display: flex; flex-direction: row; flex-wrap: wrap; align-items: center;">
+ <div class="card-hover gap-3" style="height: auto; width: 50vw; margin-top: 10%; display: flex; flex-direction: row; flex-wrap: wrap; align-items: center;">
     <div style="max-width: 85%; flex: 1;">
-		<h1 style="font-size: max(2vmin,15px);">Cards</h1>
+		<h1 style="font-size: max(2vmin,15px);">Cards & Jokers</h1>
 		<p style="text-justify: inter-word; text-align: justify; font-size: max(1.5vmin,12px);">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-			eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-			ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-			aliquip ex ea commodo consequat.
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-			eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-			ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-			aliquip ex ea commodo consequat.
+			In Nogler, your main objective is to build the strongest poker hands using a standard deck of playing cards—but the real depth comes from the jokers. 
+			These special cards don't go into your hand; instead, they sit beside it and passively enhance your plays. 
+			Some jokers multiply your score when certain hands are played, others give bonuses for using specific suits, and some completely bend the rules of the game. 
+			With hundreds of possible combinations, jokers open up endless strategic possibilities, allowing you to tailor your deck to your personal playstyle or to counter your opponents' tactics.
 		</p>
 	</div>
-	<img src='icons/pxArt.png' alt='icon' style="height: auto; width:125px; margin-right=2%">
+	<GameCard width="125px" card={{
+		rank:"A",
+		suit:"h",
+		faceUp:true,
+		overlay:0
+	}} animateCard={true}/>
  </div>
 <!-- Card 2: Shop -->
-<div class="card-hover" style="height: auto; width: 50vw; display: flex; flex-direction: row; flex-wrap: wrap; align-items: center;">
-    <img src='icons/pxArt.png' alt='icon' style="height: auto; width:125px; margin-left=2%">
+<div class="card-hover gap-3" style="height: auto; width: 50vw; display: flex; flex-direction: row; flex-wrap: wrap; align-items: center;">
+	<VoucherCard voucherId={0} width="w-[125px]" animateCard={true}/>
 	<div style="max-width: 85%; flex: 1;">
 		<h1 style="font-size: max(2vmin,15px);">Shop</h1>
 		<p style="text-justify: inter-word; text-align: justify; font-size: max(1.5vmin,12px);">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-			eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-			ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-			aliquip ex ea commodo consequat.
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-			eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-			ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-			aliquip ex ea commodo consequat.
+			Between rounds, players enter the Shop, a key phase where strategy takes shape. 
+			Here, you can spend your winnings to buy new jokers, consumables, or card packs. 
+			Each item can drastically change the flow of the game: jokers offer passive boosts, consumables let you sabotage opponents or gain temporary advantages, and card packs provide a randomized mix of both. 
+			Managing your resources wisely in the Shop is crucial—not just for building your own power, but for staying one step ahead of everyone else.
 		</p>
 	</div>
  </div>
@@ -115,20 +108,16 @@
      <div class="card-hover" style="height: auto; width: 49%; margin: 0px">
          <h1 style="font-size: max(2vmin,15px);">Friends</h1>
          <p style="text-justify: inter-word; text-align: justify; font-size: max(1.5vmin,12px);">
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-             aliquip ex ea commodo consequat.
+			Nogler lets you bring your friends into the fun with a built-in friend system. 
+			Add players, send and accept friend requests, and easily invite them to private matches—because games are always better with good company.
          </p>
      </div>
      <!-- Card 4: Compete -->
      <div class="card-hover" style="height: auto; width: 49%; margin: 0px">
          <h1 style="font-size: max(2vmin,15px);">Compete</h1>
          <p style="text-justify: inter-word; text-align: justify; font-size: max(1.5vmin,12px);">
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-             aliquip ex ea commodo consequat.
+			Step into the competition with Nogler's real-time multiplayer mode. Face off against other players in intense, strategic matches—only the smartest and boldest will climb to the top. 
+			Compete, adapt, and prove you've got what it takes to win.
          </p>
      </div>
 </div>
